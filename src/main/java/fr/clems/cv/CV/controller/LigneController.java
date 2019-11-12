25a436b1c1fc4632a290256ec9828697accff216
Clément Godard <clementgodard@free.fr>
2019-11-12 21:27:33 +0100

    [FEATURE] Ajout de la route pour rechercher en fonction de la catégorie

--- src/main/java/fr/clems/cv/CV/controller/LigneController.java
+++ src/main/java/fr/clems/cv/CV/controller/LigneController.java
@@ -2,8 +2,14 @@ package fr.clems.cv.CV.controller;
 
 import fr.clems.cv.CV.dao.LigneDAO;
 import fr.clems.cv.CV.entity.Ligne;
+import fr.clems.cv.CV.util.LigneDateComparator;
 import java.util.List;
+import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +17,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpStatusCodeException;
+import org.springframework.web.server.ResponseStatusException;
 
 @CrossOrigin(origins = "*")
 @RestController
@@ -26,9 +34,26 @@ public class LigneController {
         return this.ligneDao.findAll();
     }
     
-    @GetMapping("/{id}")
-    public Ligne getById(@PathVariable("id") Long id) {
-        return this.ligneDao.findById(id).get();
+    @GetMapping("/categorie/{categorie}")
+    public List<Ligne> getByCategorie(@PathVariable String categorie) {
+        List<Ligne> liste = this.ligneDao.getLignesByCategorie(categorie);
+        
+        // Si la date est plus longue que le traditionnel JJ/MM/AAAA
+        if (liste.size() > 0 && liste.get(0).getDate().length() > 10) {
+            liste.sort(new LigneDateComparator());
+        }
+        
+        return liste;
+    }
+    
+    @GetMapping("/{id:[\\d]+}")
+    public Ligne getById(@PathVariable("id") Long id, HttpServletResponse response) {
+        Optional<Ligne> opt = this.ligneDao.findById(id);
+        
+        if (opt.isPresent())
+            return opt.get();
+        
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Pas de ligne correspodante");
     }
     
     @PostMapping("/")
@@ -38,7 +63,7 @@ public class LigneController {
         return l.getId() > 0;
     }
     
-    @DeleteMapping("/{id}")
+    @DeleteMapping("/{id}:[\\d]+")
     public String delete(@PathVariable("id") Long id) {
         this.ligneDao.deleteById(id);
         
--- src/main/java/fr/clems/cv/CV/dao/LigneDAO.java
+++ src/main/java/fr/clems/cv/CV/dao/LigneDAO.java
@@ -1,10 +1,13 @@
 package fr.clems.cv.CV.dao;
 
 import fr.clems.cv.CV.entity.Ligne;
+import java.util.List;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 @Repository
 public interface LigneDAO extends JpaRepository<Ligne, Long>{
-    
+    @Query("SELECT l FROM Ligne l WHERE l.categorie LIKE ?1 ORDER BY l.date DESC, l.contenu ASC")
+    public List<Ligne> getLignesByCategorie(String categorie);
 }
